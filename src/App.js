b22915a69a68a75e9300bf624fddad1787df0046
João André Marques <joaoandremarques@MacBook-Pro.local>
2023-01-31 10:35:13 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import {Home, Browse, Signin, Signup} from './pages';
+import { ProtectedRoute, UserRedirect } from './helpers/routes';
+// import {useAuthListener} from './hooks';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+/// Updating Paths to V6 pattern and Firebase Modular SDK
+
+return (
+    <> 
+    <Router>
+      <Routes>
+        <Route 
+          exact path ={ROUTES.HOME}  // With V6 with need to wrap our component with our Redirect/Protected Routes
+          element={<UserRedirect path={ROUTES.BROWSE}>
+                    <Home/>
+                  </UserRedirect>}
+        />
+        <Route 
+          exact path ={ROUTES.SIGN_IN} 
+          element={<UserRedirect path={ROUTES.BROWSE}>
+                    <Signin/>
+                   </UserRedirect>
+                }
+        />
+        <Route 
+          exact path ={ROUTES.BROWSE} 
+          element={<ProtectedRoute path={ROUTES.SIGN_IN}>
+                    <Browse/>
+                  </ProtectedRoute>}
+        />
+        <Route 
+          exact path ={ROUTES.SIGN_UP} 
+          element={<UserRedirect path={ROUTES.BROWSE}>
+                    <Signup/>
+                  </UserRedirect>}
+        />
+      </Routes>
+    </Router>
+    </>
+  )
 }
-
-export default App;
